3874beff4adca630dcb7ad5275687d0b8d1fce1a
H-Taliby <habibataliby@gmail.com>
2025-10-11 14:48:24 +0100

    next js dependencies

--- src/app/Hero.js
+++ src/app/Hero.js
@@ -11,8 +11,8 @@ const playfair = Playfair_Display({
 
 export default function Hero() {
     return (
-        <div className="flex flex-row justify-between items-center w-full  px-5 py-10 gap-4 ">
-            <div className={`w-60 h-100 flex items-start justify-center grow text-3xl  font-bold text-center ${playfair.className}`}>
+        <div className="flex flex-row justify-between items-center w-full  px-5 py-10">
+            <div className={`w-60 h-100 flex items-start justify-center grow text-3xl  font-semibold text-center ${playfair.className}`}>
                 <p className="leading-[5rem] tracking-wide ">
                     Building digital worlds <br></br>
                      one<br></br>
@@ -31,6 +31,21 @@ export default function Hero() {
                 {/* an edited photo of me */}
                 <Image src={profile_pic} alt="me" width={500} height={300} className="rounded-full" />
 
+            </div>
+             <div className={`w-60 h-100  flex items-start justify-center grow text-3xl  font-semibold text-center ${playfair.className}`}>
+                <p className="leading-[5rem] tracking-wide ">
+                    Building digital worlds <br></br>
+                     one<br></br>
+                    
+                    
+
+                    Building digital worlds<br></br>
+                      one <br></br>
+                     
+                    clue at a time.<br></br>
+                   
+
+                </p>
             </div>
 
 
