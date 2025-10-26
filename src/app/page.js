ac501bddcbd61ab6f99334d5401fceab5f12a001
H-Taliby <habibataliby@gmail.com>
2025-10-26 20:52:19 +0100

    scroll to top button

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,14 @@
-
+'use client';
+import ScrollTop from "../../public/icons/SVG/scroll-totop";
 import Hero from "./components/layout/Hero";
 import AboutMe from "./components/Sections/AboutMeSection";
 import ContactMe from "./components/Sections/ContactMe";
 import ProjectsSections from "./components/Sections/ProjectsSections";
 import SkillsSections from "./components/Sections/SkillsSections";
 
+const handleScrolltoTop = ()=>{
+  window.scrollTo({top:0, behavior:'smooth'})
+}
 export default function Home() {
   return (
     // <div className="font-sans w-max-5xl mx-auto ">
@@ -15,7 +19,12 @@ export default function Home() {
         <ProjectsSections/>
 <AboutMe/>
         <ContactMe/>
-        
+         <div className=" w-full flex justify-end py-3 px-6">
+          <button onClick={()=>handleScrolltoTop()}>
+             <ScrollTop className="cursor-pointer  p-2 w-10 h-10 flex bg-[#F7A5A5] text-white rounded-full " />
+          </button>
+         
+         </div>
     </div>
          
       // </section>
