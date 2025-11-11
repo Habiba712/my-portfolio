099dbfe04d59de7bbf5549320fb24c1e58d52d81
H-Taliby <habibataliby@gmail.com>
2025-11-11 22:59:09 +0100

    edited the section header styling

--- src/app/components/layout/SectionHeader.js
+++ src/app/components/layout/SectionHeader.js
@@ -2,7 +2,7 @@
 
 export default function SectionHeader({ title, description }) {
     return (
-        <div className="mt-6 mb-6 section-header flex flex-col items-center justify-center w-full px-4 py-5 ">
+        <div className="section-header">
             <h2 className="text-center">{title}</h2>
             <p className="w-80 text-center text-sm">{description}</p>
         </div>
